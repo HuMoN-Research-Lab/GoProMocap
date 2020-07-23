25073db49d199bb7b76a0922eba9d54643deddfc
luxx0235 <54394283+luxx0235@users.noreply.github.com>
2020-07-23 09:32:27 -0400

    Update visualize.py
    updated 4th camera video

--- visualize.py
+++ visualize.py
@@ -9,28 +9,31 @@ from matplotlib.animation import FuncAnimation
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.pyplot as plt
 
-
+#Chess = np.load('CJC4Chess.npy')
 
 class Vis:
     """
     video_path:list of video path
     coords: a set of 3D coordinates with shape (# of frames,#of keypoints,3Dcoords)
     """
-    def __init__(self,left_video_path,right_video_path,mid_video_path,coords):
+    def __init__(self,left_video_path,right_video_path,mid_video_path,fouth_cam_path,coords):
         self.leftPath = left_video_path
         self.rightPath = right_video_path
         self.midPath = mid_video_path
+        self.f_path = fouth_cam_path
         self.coords = coords
-        #self.pixelCoord = pixelCoord
+        
 
     
     def display(self):
         # Create a VideoCapture object and read from input file 
         leftCap = cv2.VideoCapture(self.leftPath)
-        rightCap = cv2.VideoCapture(self.rightPath)
-        if self.midPath != None:
-            midCap = cv2.VideoCapture(self.midPath)
-
+        rightCap = cv2.VideoCapture(self.rightPath)       
+        midCap = cv2.VideoCapture(self.midPath)
+        fouthCap = cv2.VideoCapture(self.f_path)
+        
+        
+        
         #3D coordinates data and pixel data
         d = self.coords
         # p = self.pixelCoord
@@ -41,17 +44,17 @@ class Vis:
         #create subplots
         ax1 = plt.subplot(2,3,1,projection='3d') #3D
         
-        ax1.set_xlim3d([-50, 50])
-        ax1.set_xlabel('X')
+        # ax1.set_xlim3d([20, 40])
+        # ax1.set_xlabel('X')
 
-        ax1.set_ylim3d([-50,50])
-        ax1.set_ylabel('Y')
+        # ax1.set_ylim3d([20,40])
+        # ax1.set_ylabel('Y')
 
-        ax1.set_zlim3d([-50,50])
-        ax1.set_zlabel('Z')
+        # ax1.set_zlim3d([20,40])
+        # ax1.set_zlabel('Z')
 
 
-        ax2 = plt.subplot(2,3,2) #x,y,z value
+        ax2 = plt.subplot(2,3,2) #4th cam
         ax3 = plt.subplot(2,3,3) #left cam
         ax4 = plt.subplot(2,3,4) #right cam
         ax5 = plt.subplot(2,3,5) #mid cam
@@ -69,16 +72,17 @@ class Vis:
         #im1 = ax1.scatter3D(d[0,:,0], d[0,:,1], d[0,:,2], marker='o', cmap='hot',s=20)
         im15 = ax3.imshow(grab_frame(leftCap))
         im16 = ax4.imshow(grab_frame(rightCap))
-        if self.midPath != None:
-            im17 = ax5.imshow(grab_frame(midCap))
+        im17 = ax5.imshow(grab_frame(midCap))
+        im18 = ax2.imshow(grab_frame(fouthCap))
 
 
         plt.ion()
         
         c = 0
         while c < d.shape[0]:
-            temp = ax1.scatter3D(d[c,:25,0], d[c,:25,1], d[c,:25,2], marker='o', cmap='hot',s=20)
-            temp2 = ax1.scatter3D(d[c,25:,0], d[c,25:,1], d[c,25:,2], marker='o', cmap='hot',s=100)
+            #chessboard  = ax1.scatter3D(Chess[0,:54,0], Chess[0,:54,1], Chess[0,:54,2], marker='o', cmap='hot',s=20)
+            temp = ax1.scatter3D(d[c,:67,0], d[c,:67,1], d[c,:67,2], marker='o', cmap='hot',s=20)
+            temp2 = ax1.scatter3D(d[c,67:,0], d[c,67:,1], d[c,67:,2], marker='o', cmap='hot',s=100)
             
             im2, = ax1.plot(d[c,0:5,0],d[c,0:5,1],d[c,0:5,2],color = 'r',linewidth=2.0)
             im3, = ax1.plot(d[c,8:12,0],d[c,8:12,1],d[c,8:12,2],color = 'g',linewidth=2.0)
@@ -99,7 +103,11 @@ class Vis:
             im16.set_data(grab_frame(rightCap))
             if self.midPath != None:
                 im17.set_data(grab_frame(midCap))
-            plt.pause(0.01)
+            if self.f_path != None:
+                im18.set_data(grab_frame(fouthCap))
+            
+            
+            plt.pause(0.000001)
             temp.remove()
             temp2.remove()
             im2.remove()
